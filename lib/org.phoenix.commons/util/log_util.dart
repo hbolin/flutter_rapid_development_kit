05438a6e6cc1d896742f6862ac5d074a482ccde4
hbolin <hbolin@163.com>
2023-06-14 18:05:53 +0800

    LogUtil追加debug/error方法

--- lib/org.phoenix.commons/util/log_util.dart
+++ lib/org.phoenix.commons/util/log_util.dart
@@ -12,6 +12,14 @@ class LogUtil {
   /// 全局配置是否在android打印日志
   static bool configPrintInAndroid = true;
 
+  static void debug(String message, {String tag = tag, bool printInAndroid = true}) {
+    d(tag, message, printInAndroid: printInAndroid);
+  }
+
+  static void error(String message, {String tag = tag, bool printInAndroid = true}) {
+    e(tag, message, printInAndroid: printInAndroid);
+  }
+
   static void d(String tag, String message, {bool printInAndroid = true}) {
     if (kReleaseMode) {
       return;
