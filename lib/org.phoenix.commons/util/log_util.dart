0d795c12242c3d059c7b80f0f003e008133d5ffc
hbl <hbolin@163.com>
2023-08-08 22:01:39 +0800

    去掉flutter_android_log_printer

--- lib/org.phoenix.commons/util/log_util.dart
+++ lib/org.phoenix.commons/util/log_util.dart
@@ -2,12 +2,12 @@ import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter_android_log_printer/flutter_android_log_printer.dart';
+// import 'package:flutter_android_log_printer/flutter_android_log_printer.dart';
 
 class LogUtil {
   static const tag = "rapid";
 
-  static final _flutterAndroidLogPrinterPlugin = FlutterAndroidLogPrinter();
+  // static final _flutterAndroidLogPrinterPlugin = FlutterAndroidLogPrinter();
 
   /// 全局配置是否在android打印日志
   static bool configPrintInAndroid = true;
@@ -24,21 +24,23 @@ class LogUtil {
     if (kReleaseMode) {
       return;
     }
-    if (Platform.isAndroid && configPrintInAndroid && printInAndroid) {
-      _flutterAndroidLogPrinterPlugin.logD(tag, message);
-    } else {
-      log(message, name: tag);
-    }
+    log(message, name: tag);
+    // if (Platform.isAndroid && configPrintInAndroid && printInAndroid) {
+    //   _flutterAndroidLogPrinterPlugin.logD(tag, message);
+    // } else {
+    //   log(message, name: tag);
+    // }
   }
 
   static void e(String tag, String message, {bool printInAndroid = true}) {
     if (kReleaseMode) {
       return;
     }
-    if (Platform.isAndroid && configPrintInAndroid && printInAndroid) {
-      _flutterAndroidLogPrinterPlugin.logE(tag, message);
-    } else {
-      log(message, name: tag);
-    }
+    log(message, name: tag);
+    // if (Platform.isAndroid && configPrintInAndroid && printInAndroid) {
+    //   _flutterAndroidLogPrinterPlugin.logE(tag, message);
+    // } else {
+    //   log(message, name: tag);
+    // }
   }
 }
