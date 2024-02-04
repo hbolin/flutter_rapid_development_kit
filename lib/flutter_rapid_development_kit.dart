fd7decda2927462ca1889f00402adf8ba4f1500a
hbolin <hbolin@163.com>
2024-02-04 15:31:20 +0800

    log_util

--- lib/flutter_rapid_development_kit.dart
+++ lib/flutter_rapid_development_kit.dart
@@ -6,5 +6,6 @@ export 'package:flutter_rapid_development_kit/src/extension/money_format.dart';
 export 'package:flutter_rapid_development_kit/src/extension/string_number_converter.dart';
 
 export 'package:flutter_rapid_development_kit/src/util/toast_util.dart';
+export 'package:flutter_rapid_development_kit/src/util/log_util.dart';
 
 export 'package:flutter_rapid_development_kit/src/widget/cached_loading_body.dart';
---
+++ lib/src/util/log_util.dart
@@ -0,0 +1,39 @@
+import 'dart:developer';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_android_log_printer/flutter_android_log_printer.dart';
+import 'package:universal_platform/universal_platform.dart';
+
+class LogUtil {
+  static final _flutterAndroidLogPrinterPlugin = FlutterAndroidLogPrinter();
+
+  static const _tag = "rapid";
+
+  /// 打印debug日志
+  static void debug(String message, {String tag = _tag}) {
+    if (kReleaseMode) {
+      return;
+    }
+    if (UniversalPlatform.isAndroid) {
+      _flutterAndroidLogPrinterPlugin.logD(tag, message);
+    } else if (UniversalPlatform.isIOS) {
+      log(message, name: tag);
+    } else {
+      debugPrint(message);
+    }
+  }
+
+  /// 打印error日志
+  static void error(String message, {String tag = _tag}) {
+    if (kReleaseMode) {
+      return;
+    }
+    if (UniversalPlatform.isAndroid) {
+      _flutterAndroidLogPrinterPlugin.logE(tag, message);
+    } else if (UniversalPlatform.isIOS) {
+      log(message, name: tag);
+    } else {
+      debugPrint(message);
+    }
+  }
+}
