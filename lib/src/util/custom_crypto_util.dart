f0bae2e503dc7b2ef08dc65ad68a5a1248613987
zhangwu <hbolin@163.com>
2025-01-22 16:01:10 +0800

    CustomCryptoUtil

--- lib/src/util/custom_crypto_util.dart
+++ lib/src/util/custom_crypto_util.dart
@@ -3,15 +3,11 @@ import 'dart:convert';
 class CustomCryptoUtil {
   /// 自定义Base64 - 加密
   static String encryptBase64(String src) {
-    // 上面这种会出现问题。不能使用
-    // return base64Encode(utf8.encode(src).map((e) => e ^ 0xcc).toList());
-    return base64Encode(utf8.encode(src).toList());
+    return base64Url.encode(utf8.encode(src));
   }
 
   /// 自定义Base64 - 解密
   static String decryptBase64(String src) {
-    // 上面这种会出现问题。不能使用
-    // return utf8.decode(base64Decode(src).map((e) => e ^ 0xcc).toList());
-    return utf8.decode(base64Decode(src).toList());
+    return utf8.decode(base64Url.decode(src));
   }
 }
