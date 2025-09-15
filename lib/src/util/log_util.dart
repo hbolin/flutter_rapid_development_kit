aa5ce0741ba9595fe122cda9f64d2bea8913ec68
zhangwu <hbolin@163.com>
2025-09-15 15:58:26 +0800

    LogUtil

--- lib/src/util/log_util.dart
+++ lib/src/util/log_util.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/foundation.dart';
-import 'package:logger/logger.dart';
+import 'package:flutter_rapid_development_kit/flutter_rapid_development_kit.dart';
 import 'package:universal_platform/universal_platform.dart';
 
 class LogUtil {
@@ -7,8 +7,6 @@ class LogUtil {
 
   static const _tag = "rapid";
   static bool _isLogEnabled = !kReleaseMode; // 全局日志开关
-
-  static final Logger _logger = Logger();
 
   /// 启用或禁用日志
   static void enableLog(bool isEnabled) {
@@ -21,7 +19,19 @@ class LogUtil {
       return;
     }
     if (UniversalPlatform.isAndroid) {
-      _logger.d(message); // 使用 debug 级别
+      int maxSize = 512;
+      if (message.length > maxSize) {
+        final chunks = message.chunk(maxSize);
+        for (int i = 0; i < chunks.length; i++) {
+          if (i == 0) {
+            print('[38;5;39m[${DateTime.now()}] [$tag] ${chunks[i]} [0m');
+          } else {
+            print('[38;5;39m${chunks[i]} [0m');
+          }
+        }
+      } else {
+        print('[38;5;39m[${DateTime.now()}] [$tag] $message [0m');
+      }
     } else if (UniversalPlatform.isIOS) {
       debugPrint(message);
     } else {
@@ -35,11 +45,37 @@ class LogUtil {
       return;
     }
     if (UniversalPlatform.isAndroid) {
-      _logger.e(message); // 使用 error 级别
+      int maxSize = 512;
+      if (message.length > maxSize) {
+        final chunks = message.chunk(maxSize);
+        for (int i = 0; i < chunks.length; i++) {
+          if (i == 0) {
+            print('[38;5;203m[${DateTime.now()}] [$tag] ${chunks[i]} [0m');
+          } else {
+            print('[38;5;203m${chunks[i]} [0m');
+          }
+        }
+      } else {
+        print('[38;5;203m[${DateTime.now()}] [$tag] $message [0m');
+      }
     } else if (UniversalPlatform.isIOS) {
       debugPrint(message);
     } else {
       debugPrint(message);
     }
+  }
+}
+
+extension _ on String {
+  /// 将字符串按指定长度分块
+  List<String> chunk([int size = 512]) {
+    if (isEmpty) return [];
+
+    final chunks = <String>[];
+    for (var i = 0; i < length; i += size) {
+      final end = (i + size).clamp(0, length);
+      chunks.add(substring(i, end));
+    }
+    return chunks;
   }
 }
